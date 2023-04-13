0bc2a6771f52d16bbe2dd14fff3d2b5b2d8fd364
shaileshkachare <shaileshkachare2@gmail.com>
2023-04-13 22:16:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+// const fs = require("fs/promises");
+// const fileName = "myfile.txt";
+// const fileContent = "Newton School";
+// const writeFile = async (fileName, fileContent) => {
+//   // write code here
+//   // dont change function name
+// };
+
+// module.exports = { writeFile };
+
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  await fs.writeFile(fileName, fileContent)
 };
 
 module.exports = { writeFile };
